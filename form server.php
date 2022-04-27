41c4355d5a0d199e8037429528aae321f9ad14ba
kaijun123 <96715775+kaijun123@users.noreply.github.com>
2022-04-28 02:18:59 +0800

    Add files via upload

---
+++ form server.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Dear <?php echo $_POST["name"]; ?>,<br>
+The data you have inputted is as follows:
+Email: <?php echo $_POST["email"]; ?>
+Contact Number: <?php echo $_POST["contact number"]; ?>
+Message: <?php echo $_POST["message"]; ?>
+
+
+</body>
+</html>
+
